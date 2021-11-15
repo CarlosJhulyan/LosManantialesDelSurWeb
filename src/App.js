ebec88b28f14add677eef5e40e589b4b4253fac2
CarlosJhulyan <cmedinallantirhuay@gmail.com>
2021-11-15 12:14:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto Aplicación Web con React.js para curso de Sistemas Distribuidos Ingeniería de Sistemas serie 300 UNSCH
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
