8c4335b1791ddced5e4ed2b826a7bb59fc0c8b27
CarlosJhulyan <cmedinallantirhuay@gmail.com>
2021-11-19 11:17:57 -0500

    Home.jsx fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,6 @@ import AboutUs from "../components/AboutUs";
 import OurAgencies from "../components/OurAgencies";
 import OurServices from "../components/OurServices";
 import Access from "../components/Access";
-import Access from "../pages/Dashboard";
-import Access from "../pages/PackageTracking";
-import Access from "../pages/Passage";
-import Access from "../pages/Registration";
-import Access from "../pages/RegistrationPackage";
-import Access from "../pages/RegistrationPayment";
-import Access from "../pages/TypeServices";
-import Access from "../pages/UpdateCliente";
 
 
 const Home = () => {
